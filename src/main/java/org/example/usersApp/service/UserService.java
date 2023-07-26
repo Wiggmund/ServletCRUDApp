90678bb7d8eaffc5e7a313a1d12db201f50fd7ca
StefanSteuge <fotosteuge@gmail.com>
2023-07-26 23:43:28 +0300

    Covered getUserById in Optional

--- src/main/java/org/example/usersApp/service/UserService.java
+++ src/main/java/org/example/usersApp/service/UserService.java
@@ -4,12 +4,13 @@ import org.example.usersApp.dto.CreateUserDto;
 import org.example.usersApp.dto.UpdateUserDto;
 import org.example.usersApp.model.User;
 
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
 public interface UserService {
     List<User> getAllUsers();
-    Optional<User> getUserById(Long userId);
+    Optional<User> getUserById(Long userId) throws SQLException;
     User createUser(CreateUserDto dto);
     User updateUser(UpdateUserDto dto);
     User deleteUserById(Long userId);
