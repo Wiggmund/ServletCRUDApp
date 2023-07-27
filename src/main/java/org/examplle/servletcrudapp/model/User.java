978265c0d5dcbdc39aa1580c60914d54f82b1060
StefanSteuge <fotosteuge@gmail.com>
2023-07-27 14:42:16 +0300

    add getters and setters

--- src/main/java/org/examplle/servletcrudapp/model/User.java
+++ src/main/java/org/examplle/servletcrudapp/model/User.java
@@ -1,4 +1,4 @@
-package org.example.usersApp.model;
+package org.examplle.servletcrudapp.model;
 
 public class User {
     private Long id;
@@ -13,6 +13,38 @@ public class User {
     }
 
     public User(Long userId, String firstName, String lastName, String age) {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
     }
 
     @Override
