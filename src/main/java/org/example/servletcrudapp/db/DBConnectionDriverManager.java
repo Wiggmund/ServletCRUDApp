8cdc187a8f179d8a660a71fb96803b1e67b858e2
Aliaksandr Matsiushonak <shaman198786@gmail.com>
2023-07-27 18:21:17 +0100

    url

--- src/main/java/org/example/servletcrudapp/db/DBConnectionDriverManager.java
+++ src/main/java/org/example/servletcrudapp/db/DBConnectionDriverManager.java
@@ -5,10 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnectionDriverManager implements DBConnection {
-    private final String URL = "jdbc:postgresql://192.168.1.87:5432/servletcrud";
+    private final String URL = "jdbc:postgresql://localhost/servletcrud";
     private final String USERNAME = "postgres";
-    private final String PASSWORD = "05051987";
-
+    private final String PASSWORD = "root";
 
 
     @Override
