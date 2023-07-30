f10f5e7430a055f6285e2ac5b938d8197b72f10d
Wiggmund <mirracleourworld@gmail.com>
2023-07-30 16:26:02 +0300

    Change DB IP and password for Alex

--- src/main/java/org/example/servletcrudapp/db/DBConnectionDriverManager.java
+++ src/main/java/org/example/servletcrudapp/db/DBConnectionDriverManager.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnectionDriverManager implements DBConnection {
-    private final String URL = "jdbc:postgresql://localhost:5432/servletcrud";
+    private final String URL = "jdbc:postgresql://192.168.1.87:5432/servletcrud";
     private final String USERNAME = "postgres";
-    private final String PASSWORD = "root";
+    private final String PASSWORD = "05051987";
 
     @Override
     public Connection getConnection() throws SQLException {
