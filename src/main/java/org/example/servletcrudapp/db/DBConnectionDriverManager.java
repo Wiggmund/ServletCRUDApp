b5c654b1874fd07c4b94dc42104a446cac6904d5
Wiggmund <mirracleourworld@gmail.com>
2023-07-30 23:14:46 +0300

    Change ip and password back

--- src/main/java/org/example/servletcrudapp/db/DBConnectionDriverManager.java
+++ src/main/java/org/example/servletcrudapp/db/DBConnectionDriverManager.java
@@ -5,11 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnectionDriverManager implements DBConnection {
-    // private final String URL = "jdbc:postgresql://192.168.1.87:5432/servletcrud";
-    private final String URL = "jdbc:postgresql://localhost:5432/servletcrud";
-    private final String USERNAME = "postgres";
-    // private final String PASSWORD = "05051987";
-    private final String PASSWORD = "root";
+    private static final String URL = "jdbc:postgresql://192.168.1.87:5432/servletcrud";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "05051987";
 
     @Override
     public Connection getConnection() throws SQLException {
