ec649f8c85ff3da4d5cd3e6500057149d6fffa11
bukinpavel <bukinpavelvl@yandex.ru>
2023-07-30 19:22:35 +0400

    fixes errors

--- src/main/java/org/example/servletcrudapp/exception/ExceptionResponse.java
+++ src/main/java/org/example/servletcrudapp/exception/ExceptionResponse.java
@@ -2,8 +2,38 @@ package org.example.servletcrudapp.exception;
 
 import java.time.LocalDateTime;
 
-public record ExceptionResponse (
-        String message,
-        int statusCode,
-        LocalDateTime timestamp
-) {}
+public class ExceptionResponse {
+    private String message;
+    private int statusCode;
+    private LocalDateTime timestamp;
+
+    public ExceptionResponse(String message, int statusCode, LocalDateTime timestamp) {
+        this.message = message;
+        this.statusCode = statusCode;
+        this.timestamp = timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+}
